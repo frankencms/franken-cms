77592aa0c0a7b9aded7f07d3e4959b5da15a0df4
Mike Wall <daikazu@gmail.com>
2025-09-22 12:37:39 -0400

    wip

--- src/Filament/Resources/Menus/Pages/ListMenus.php
+++ src/Filament/Resources/Menus/Pages/ListMenus.php
@@ -6,16 +6,17 @@ namespace FrankenCms\Filament\Resources\Menus\Pages;
 
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Tables\Table;
 use FrankenCms\Filament\Resources\Menus\MenuResource;
-use Illuminate\Database\Eloquent\Builder;
 
 class ListMenus extends ListRecords
 {
     protected static string $resource = MenuResource::class;
 
-    protected function getTableQuery(): Builder
+    public function table(Table $table): Table
     {
-        return parent::getTableQuery()->withCount('allMenuItems');
+        return parent::table($table)
+            ->modifyQueryUsing(fn ($query) => $query->withCount('allMenuItems'));
     }
 
     protected function getHeaderActions(): array
