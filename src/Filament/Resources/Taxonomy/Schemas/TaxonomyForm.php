3da1478cbd18cf4ae1623703021b94497751c824
Mike Wall <daikazu@gmail.com>
2025-10-28 16:39:09 -0400

    wip

--- src/Filament/Resources/Taxonomy/Schemas/TaxonomyForm.php
+++ src/Filament/Resources/Taxonomy/Schemas/TaxonomyForm.php
@@ -6,6 +6,7 @@ namespace FrankenCms\Filament\Resources\Taxonomy\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class TaxonomyForm
@@ -13,9 +14,23 @@ class TaxonomyForm
     public static function make(Schema $schema): Schema
     {
         return $schema
+            ->columns(1)
             ->components([
-                TextInput::make('name')->required(),
-                Toggle::make('hierarchical')->label('Hierarchical?'),
+                Section::make('Taxonomy Details')
+                    ->description('Define the taxonomy name and structure type')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Name')
+                            ->inlineLabel()
+                            ->required()
+                            ->maxLength(255),
+
+                        Toggle::make('hierarchical')
+                            ->label('Hierarchical')
+                            ->inlineLabel()
+                            ->helperText('Enable parent-child relationships for this taxonomy')
+                            ->default(false),
+                    ]),
             ]);
     }
 }
--- src/Filament/Resources/Term/Schemas/TermForm.php
+++ src/Filament/Resources/Term/Schemas/TermForm.php
@@ -7,6 +7,7 @@ namespace FrankenCms\Filament\Resources\Term\Schemas;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
@@ -21,30 +22,53 @@ class TermForm
         return $schema
             ->columns(1)
             ->components([
-                TextInput::make('name')
-                    ->inlineLabel()
-                    ->live()
-                    ->required(),
-                TextInput::make('slug')
-                    ->inlineLabel()
-                    ->afterStateUpdated(fn (Set $set, $state) => $set('slug', Str::slug($state)))
-                    ->unique(ignoreRecord: true),
-                Textarea::make('description')
-                    ->label('Description')
-                    ->inlineLabel()
-                    ->rows(3)
-                    ->nullable()
-                    ->columnSpanFull(),
-                Select::make('taxonomy_id')
-                    ->label('Taxonomy')
-                    ->inlineLabel()
-                    ->options(Taxonomy::all()->pluck('name', 'id'))
-                    ->required(),
-                Select::make('parent_id')
-                    ->label('Parent Term')
-                    ->inlineLabel()
-                    ->options(fn (Get $get) => Term::where('taxonomy_id', $get('taxonomy_id'))->pluck('name', 'id'))
-                    ->searchable(),
+                Section::make('Term Details')
+                    ->description('Configure the term name, slug, and taxonomy')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Name')
+                            ->inlineLabel()
+                            ->required()
+                            ->maxLength(255)
+                            ->live(onBlur: true)
+                            ->afterStateUpdated(function (string $context, $state, Set $set) {
+                                if ($context === 'create') {
+                                    $set('slug', Str::slug($state));
+                                }
+                            }),
+
+                        TextInput::make('slug')
+                            ->label('Slug')
+                            ->inlineLabel()
+                            ->required()
+                            ->maxLength(255)
+                            ->unique(Term::class, 'slug', ignoreRecord: true)
+                            ->helperText('URL-friendly version of the name'),
+
+                        Select::make('taxonomy_id')
+                            ->label('Taxonomy')
+                            ->inlineLabel()
+                            ->options(Taxonomy::all()->pluck('name', 'id'))
+                            ->required()
+                            ->searchable()
+                            ->live(),
+
+                        Select::make('parent_id')
+                            ->label('Parent Term')
+                            ->inlineLabel()
+                            ->options(fn (Get $get) => Term::where('taxonomy_id', $get('taxonomy_id'))->pluck('name', 'id'))
+                            ->searchable()
+                            ->helperText('Optional: Set a parent term to create a hierarchy')
+                            ->visible(fn (Get $get) => filled($get('taxonomy_id'))),
+
+                        Textarea::make('description')
+                            ->label('Description')
+                            ->inlineLabel()
+                            ->rows(3)
+                            ->maxLength(500)
+                            ->helperText('Optional description for this term')
+                            ->columnSpanFull(),
+                    ]),
             ]);
     }
 }
