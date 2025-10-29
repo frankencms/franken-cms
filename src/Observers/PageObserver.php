a9c2948137b31f964ab0f54e8aef8cc67a9a22d0
Mike Wall <daikazu@gmail.com>
2025-10-28 20:18:37 -0400

    wip

--- src/Observers/PageObserver.php
+++ src/Observers/PageObserver.php
@@ -3,6 +3,7 @@
 namespace FrankenCms\Observers;
 
 use FrankenCms\Models\Page;
+use FrankenCms\Services\PageRouteService;
 
 class PageObserver
 {
@@ -18,6 +19,12 @@ class PageObserver
         }
     }
 
+    public function created(Page $post): void
+    {
+        // Clear route cache after page is created
+        app(PageRouteService::class)->clearCache();
+    }
+
     public function updating(Page $post): void
     {
         // If route_name is empty during update, fill it from slug
@@ -25,5 +32,19 @@ class PageObserver
         if (empty($post->route_name) && ! empty($post->post_slug)) {
             $post->route_name = $post->post_slug;
         }
+    }
+
+    public function updated(Page $post): void
+    {
+        // Clear route cache when page is updated (route_name, slug, or status might change)
+        if ($post->wasChanged(['route_name', 'post_slug', 'post_status', 'parent_id'])) {
+            app(PageRouteService::class)->clearCache();
+        }
+    }
+
+    public function deleted(Page $post): void
+    {
+        // Clear route cache when page is deleted
+        app(PageRouteService::class)->clearCache();
     }
 }
