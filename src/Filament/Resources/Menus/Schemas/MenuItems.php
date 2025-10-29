79c4b0a59095c4cab12af00b0f2c5ab047fe2bb0
Mike Wall <daikazu@gmail.com>
2025-10-28 20:34:39 -0400

    wip

--- src/Filament/Resources/Menus/Schemas/MenuItems.php
+++ src/Filament/Resources/Menus/Schemas/MenuItems.php
@@ -11,6 +11,7 @@ use Filament\Schemas\Components\Form;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use FrankenCms\Enums\LinkTargets;
+use FrankenCms\Models\Page;
 use FrankenCms\Models\Post;
 
 class MenuItems
@@ -31,40 +32,101 @@ class MenuItems
                                         ->required()
                                         ->maxLength(255),
 
+                                    Select::make('link_to')
+                                        ->label('Link To')
+                                        ->inlineLabel()
+                                        ->options(function () {
+                                            $options = ['custom' => 'Custom URL'];
+
+                                            // Get all pages (Page model has PageScope that filters by post_type='page')
+                                            $pages = Page::query()
+                                                ->where('post_status', 'published')
+                                                ->orderBy('post_title')
+                                                ->get();
+
+                                            $pageOptions = [];
+                                            foreach ($pages as $page) {
+                                                $pageOptions['page:' . $page->id] = $page->post_title;
+                                            }
+                                            if (! empty($pageOptions)) {
+                                                $options['Pages'] = $pageOptions;
+                                            }
+
+                                            // Get all posts (explicitly filter by post_type='post')
+                                            $posts = Post::query()
+                                                ->where('post_type', 'post')
+                                                ->where('post_status', 'published')
+                                                ->orderBy('post_title')
+                                                ->get();
+
+                                            $postOptions = [];
+                                            foreach ($posts as $post) {
+                                                $postOptions['post:' . $post->id] = $post->post_title;
+                                            }
+                                            if (! empty($postOptions)) {
+                                                $options['Posts'] = $postOptions;
+                                            }
+
+                                            return $options;
+                                        })
+                                        ->searchable()
+                                        ->live()
+                                        ->default(function (callable $get) {
+                                            // Populate from existing linkable data
+                                            $linkableType = $get('linkable_type');
+                                            $linkableId = $get('linkable_id');
+                                            $url = $get('url');
+
+                                            if ($url) {
+                                                return 'custom';
+                                            }
+
+                                            if ($linkableType && $linkableId) {
+                                                if (str_contains($linkableType, 'Page')) {
+                                                    return 'page:' . $linkableId;
+                                                } elseif (str_contains($linkableType, 'Post')) {
+                                                    return 'post:' . $linkableId;
+                                                }
+                                            }
+
+                                            return null;
+                                        })
+                                        ->afterStateUpdated(function (callable $set, $state) {
+                                            // Clear URL when switching from custom
+                                            if ($state !== 'custom') {
+                                                $set('url', null);
+                                            }
+
+                                            // Parse selection and set linkable fields
+                                            if ($state && $state !== 'custom') {
+                                                [$type, $id] = explode(':', $state);
+                                                $set('linkable_type', $type === 'page' ? Page::class : Post::class);
+                                                $set('linkable_id', (int) $id);
+                                            } else {
+                                                $set('linkable_type', null);
+                                                $set('linkable_id', null);
+                                            }
+                                        })
+                                        ->dehydrated(false)
+                                        ->helperText('Select a page/post or choose Custom URL'),
+
                                     TextInput::make('url')
                                         ->label('Custom URL')
                                         ->inlineLabel()
                                         ->url()
                                         ->placeholder('https://example.com')
-                                        ->helperText('Leave empty to use route or linkable model'),
+                                        ->visible(fn (callable $get) => $get('link_to') === 'custom')
+                                        ->required(fn (callable $get) => $get('link_to') === 'custom')
+                                        ->helperText('Enter the full URL including https://'),
 
-                                    TextInput::make('route_name')
-                                        ->label('Route Name')
-                                        ->inlineLabel()
-                                        ->placeholder('post.show')
-                                        ->helperText('Laravel route name'),
+                                    // Hidden fields to store the polymorphic relationship
+                                    TextInput::make('linkable_type')
+                                        ->hidden()
+                                        ->dehydrated(),
 
-                                    Select::make('linkable_type')
-                                        ->label('Link Type')
-                                        ->inlineLabel()
-                                        ->options([
-                                            ''          => 'No Link',
-                                            Post::class => 'Post',
-                                        ])
-                                        ->live(),
-
-                                    Select::make('linkable_id')
-                                        ->label('Select Content')
-                                        ->inlineLabel()
-                                        ->options(function (callable $get) {
-                                            $type = $get('linkable_type');
-                                            if ($type === Post::class) {
-                                                return Post::pluck('post_title', 'id')->toArray();
-                                            }
-                                            return [];
-                                        })
-                                        ->searchable()
-                                        ->visible(fn (callable $get) => ! empty($get('linkable_type'))),
+                                    TextInput::make('linkable_id')
+                                        ->hidden()
+                                        ->dehydrated(),
 
                                     Select::make('target')
                                         ->label('Link Target')
